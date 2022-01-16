aca907f7a497b94afcf665b09bebe9417a20b037
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-01-16 15:19:35 +0700

    Create BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,7 @@
+package com.dicoding.javafundamental.bangundatar;
+
+public interface BangunDatar {
+    public double luas(int s1, int s2);
+    public double luas1(int d1, int d2);
+    public double keliling(int s1, int s2);
+}
