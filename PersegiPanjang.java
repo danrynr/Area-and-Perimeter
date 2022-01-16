822f4ee14761dbf3084ab1b4137473305e06683b
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-01-16 15:20:07 +0700

    Create PersegiPanjang.java

---
+++ PersegiPanjang.java
@@ -0,0 +1,18 @@
+public class PersegiPanjang implements BangunDatar {
+    PersegiPanjang() {}
+
+    @Override
+    public double luas(int s1, int s2) {
+        return (s1 * s2);
+    }
+
+    @Override
+    public double luas1(int d1, int d2) {
+        return 0;
+    }
+
+    @Override
+    public double keliling(int s1, int s2) {
+        return (2 * (s1 + s2));
+    }
+}
