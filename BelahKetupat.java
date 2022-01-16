db3e425b04068ff902ca5e0866dce21a3e03781e
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-01-16 15:22:09 +0700

    Create BelahKetupat.java

---
+++ BelahKetupat.java
@@ -0,0 +1,20 @@
+public class BelahKetupat implements BangunDatar {
+    BelahKetupat() {}
+
+    @Override
+    public double luas(int s1, int s2) {
+        return 0;
+    }
+
+    @Override
+    public double luas1(int d1, int d2) {
+        return (0.5 * d1 * d2);
+    }
+
+    @Override
+    public double keliling(int s1, int s2) {return 0;}
+
+    public double keliling1(int s5) {
+        return (s5 * 4);
+    }
+}
