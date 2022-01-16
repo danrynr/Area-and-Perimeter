b49636af81de4d583aac3c694937f24424cfa11a
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-01-16 15:21:41 +0700

    Create Layang.java

---
+++ Layang.java
@@ -0,0 +1,18 @@
+public class Layang implements BangunDatar {
+    Layang() {}
+
+    @Override
+    public double luas(int s1, int s2) {
+        return 0;
+    }
+
+    @Override
+    public double luas1(int d1, int d2) {
+        return (0.5 * d1 * d2);
+    }
+
+    @Override
+    public double keliling(int s1, int s2) {
+        return (2 * (s1 + s2));
+    }
+}
